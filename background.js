999788bbc76849f6041a33c5543fd8b4a8f7c121
unknown <tomerpacific@gmail.com>
2021-05-08 22:14:13 +0300

    Youtube-pause logic to listen in on youtube pages

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
-chrome.runtime.onInstalled.addListener(() => {
-    
-  });
+var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?youtube\.com/;
+
+function addVideoPausedListener(domContent) {
+
+}
+
+
+chrome.runtime.onInstalled.addListener((tab) => {
+    if (urlRegex.test(tab.url)) {
+      chrome.tabs.sendMessage(tab.id, {text: 'inner_message'}, addVideoPausedListener);
+    }
+});
